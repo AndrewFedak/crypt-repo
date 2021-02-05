37663d5376434b3432fc2cd60346fc5ff1d3ed30
Andrii Fedak <Andrii_Fedak@epam.com>
2021-02-05 23:38:43 +0200

    ininit

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './root/root';
+
+const mountNode = document.getElementById('app');
+
+ReactDOM.render(
+  <Root />, mountNode,
+);
---
+++ src/root/root.jsx
@@ -0,0 +1,11 @@
+import '../index.scss';
+import React from 'react';
+
+const App = () => (
+  <div className="App">
+    <p>123</p>
+    <p>123</p>
+  </div>
+);
+
+export default App;
